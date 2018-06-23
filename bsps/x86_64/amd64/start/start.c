49276283fe011e3cf100054919f6626b75f94230
Amaan Cheval <amaan.cheval@gmail.com>
2018-06-23 15:08:34 +0530

    start.c: Add header definition for _start

--- bsps/x86_64/amd64/start/start.c
+++ bsps/x86_64/amd64/start/start.c
@@ -1,6 +1,8 @@
 // XXX: _start can pass boot arguments to boot_card
 // https://lists.rtems.org/pipermail/devel/2018-June/022123.html
-void _start()
+void _start(void);
+
+void _start(void)
 {
   boot_card("");
 }
