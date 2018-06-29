ecd494da8b184dd7a3bd487ee429aad79ddd363e
Amaan Cheval <amaan.cheval@gmail.com>
2018-06-29 14:42:59 +0530

    Revert "HACK: Quickly test COM1 / UART setup on x64"
    This reverts commit 6a8daacdf98e55f40dbb9072e29be08f47693839.

--- testsuites/samples/hello/init.c
+++ testsuites/samples/hello/init.c
@@ -15,45 +15,18 @@
 #include <tmacros.h>
 
 const char rtems_test_name[] = "HELLO WORLD";
-void init_serial();
-void write_serial(char a);
+
 static rtems_task Init(
   rtems_task_argument ignored
 )
 {
-  /*
-   * rtems_print_printer_fprintf_putc(&rtems_test_printer);
-   * TEST_BEGIN();
-   * printf( "Hello World\n" );
-   * TEST_END();
-   * rtems_test_exit( 0 );
-   */
-  init_serial();
-  write_serial('h');
-  write_serial('e');
-  write_serial('l');
-  write_serial('l');
-  write_serial('o');
-  write_serial(' ');
-  write_serial('w');
-  write_serial('o');
-  write_serial('r');
-  write_serial('l');
-  write_serial('d');
-  write_serial(' ');
-  write_serial('o');
-  write_serial('n');
-  write_serial(' ');
-  write_serial('x');
-  write_serial('6');
-  write_serial('4');
-  write_serial('!');
-
+  rtems_print_printer_fprintf_putc(&rtems_test_printer);
+  TEST_BEGIN();
+  printf( "Hello World\n" );
+  TEST_END();
+  rtems_test_exit( 0 );
 }
 
-// XXX
-//#define CONFIGURE_APPLICATION_DISABLE_FILESYSTEM
-//#define CONFIGURE_HAS_OWN_CONFIGURATION_TABLE
 
 /* NOTICE: the clock driver is explicitly disabled */
 #define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
