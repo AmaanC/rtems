0022d77633309c7a2abe69b6b346c4ac1ea3fc50
Amaan Cheval <amaan.cheval@gmail.com>
2018-07-04 19:31:58 +0530

    Use inline function for inb, outb instead of macro and misc. cleanup

--- bsps/x86_64/amd64/console/console.c
+++ bsps/x86_64/amd64/console/console.c
@@ -1,8 +1,6 @@
 /*
- *  This file contains the template for a console IO package.
- *
  *  COPYRIGHT (c) 2018.
- *  Amaan Cheval <amaan.cheval@gmail.com>
+ *  Amaan Cheval <amaan.cheval@gmail.com>.
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
@@ -13,48 +11,45 @@
 #include <rtems/bspIo.h>
 #include <bsp.h>
 #include <bsp/console-termios.h>
+#include <rtems/score/cpuimpl.h>
 
 static uint8_t amd64_uart_get_register(uintptr_t addr, uint8_t i)
 {
-  register uint8_t val;
-
-  inport_byte( (addr + i), val );
-  return val;
+  return inport_byte(addr + i);
 }
 
 static void amd64_uart_set_register(uintptr_t addr, uint8_t i, uint8_t val)
 {
-  outport_byte( (addr + i), val );
+  outport_byte(addr + i, val);
 }
 
 static ns16550_context amd64_uart_context = {
   .base = RTEMS_TERMIOS_DEVICE_CONTEXT_INITIALIZER("UART"),
   .get_reg = amd64_uart_get_register,
   .set_reg = amd64_uart_set_register,
-  .port = (uintptr_t) 0x3F8,
-//  .irq = NULL,
-  .initial_baud = (115200 * 16)
+  .port = (uintptr_t) COM1_BASE_IO,
+  .initial_baud = COM1_CLOCK_RATE
 };
 
-
+/*
+ * XXX: We should use the interrupt based handler once interrupts are supported
+ */
 const console_device console_device_table[] = {
     {
       .device_file = "/dev/console",
       .probe = console_device_probe_default,
       .handler = &ns16550_handler_polled,
       .context = &amd64_uart_context.base
-    },
+    }
 };
-
 const size_t console_device_count = RTEMS_ARRAY_SIZE(console_device_table);
 
 static void output_char(char c)
 {
   rtems_termios_device_context *ctx = console_device_table[0].context;
 
-  ns16550_polled_putchar( ctx, c );
+  ns16550_polled_putchar(ctx, c);
 }
 
 BSP_output_char_function_type BSP_output_char = output_char;
-
 BSP_polling_getchar_function_type BSP_poll_char = NULL;
--- cpukit/score/cpu/x86_64/include/rtems/score/x86_64.h
+++ cpukit/score/cpu/x86_64/include/rtems/score/x86_64.h
@@ -5,41 +5,11 @@
 extern "C" {
 #endif
 
-/*
- *  This file contains the information required to build
- *  RTEMS for a particular member of the NO CPU family.
- *  It does this by setting variables to indicate which
- *  implementation dependent features are present in a particular
- *  member of the family.
- *
- *  This is a good place to list all the known CPU models
- *  that this port supports and which RTEMS CPU model they correspond
- *  to.
- */
-
-/*
- *  Define the name of the CPU family.
- */
-
 #define CPU_NAME "x86-64"
 #define CPU_MODEL_NAME "XXX: x86-64 generic"
 
-#define inport_byte( _port, _value )                \
-  do { unsigned short __port  = _port;              \
-    unsigned char  __value = 0;                     \
-                                                    \
-    __asm__ volatile ( "inb %1,%0" : "=a" (__value) \
-                       : "d"  (__port)              \
-      );                                            \
-    _value = __value;                               \
-  } while (0)
-
-#define outport_byte( _port, _value )                                   \
-  do { unsigned short __port  = _port;                                  \
-    unsigned char  __value = _value;                                    \
-                                                                        \
-    __asm__ volatile ( "outb %0,%1" : : "a" (__value), "d" (__port) );  \
-  } while (0)
+#define COM1_BASE_IO 0x3F8
+#define COM1_CLOCK_RATE (115200 * 16)
 
 #ifdef __cplusplus
 }
